ed4a4b1410167e38062ea0b147bd3f6fc39203b3
gabrielbarth <gabriel.silverio@stone.com.br>
2026-06-13 11:57:24 -0400

    add world cup project

--- scripts/seedFirestore.ts
+++ scripts/seedFirestore.ts
@@ -1,12 +1,13 @@
 import { initializeApp, cert } from 'firebase-admin/app'
 import { getFirestore, Timestamp } from 'firebase-admin/firestore'
 import * as dotenv from 'dotenv'
+import { createRequire } from 'module'
 import { SEED_TEAMS } from '../src/data/seedTeams'
 import { SEED_MATCHES } from '../src/data/seedMatches'
 
 dotenv.config({ path: '.env.local' })
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+const require = createRequire(import.meta.url)
 const serviceAccount = require('../serviceAccountKey.json')
 
 initializeApp({ credential: cert(serviceAccount) })
